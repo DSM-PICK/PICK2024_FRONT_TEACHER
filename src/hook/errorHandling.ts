1aede3a745ce997ce49b2593de5a97dc3c5aa2f1
hyuna <hyuna_0525@dsm.hs.kr>
2024-07-14 18:42:19 +0900

    chore : 리프레쉬토큰

--- src/hook/errorHandling.ts
+++ src/hook/errorHandling.ts
@@ -8,14 +8,11 @@ type HandleType = {
 };
 
 const apiError = () => {
-  const router = useRouter();
   const handle400: ErrorHandler = () => {
     alert("400 잘못된 요청입니다");
   };
 
-  const handle401: ErrorHandler = () => {
-    router.push("login");
-  };
+  const handle401: ErrorHandler = () => {};
 
   const handle403: ErrorHandler = () => {
     alert("403 권한이 없습니다");
